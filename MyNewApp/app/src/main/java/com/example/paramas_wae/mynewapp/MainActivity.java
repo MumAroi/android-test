65cbc7ca6a79772b60ec5e74bd5b4a269182eb1a
Paramest <paramest.wsw@gmail.com>
2017-05-25 17:20:24 +0700

    test buttononClick

--- MyNewApp/app/src/main/java/com/example/paramas_wae/mynewapp/MainActivity.java
+++ MyNewApp/app/src/main/java/com/example/paramas_wae/mynewapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.paramas_wae.mynewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttononClick(View v){
+        TextView textView = (TextView) findViewById(R.id.message);
+
+        textView.setText("Button Clicked");
+
     }
 }
