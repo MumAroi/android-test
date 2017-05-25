c25c58d58a7911f67504503acc76e567edf03f5f
paramest.wsw <paramest.wsw@gmail.com>
2017-05-25 21:38:11 +0700

    BMI layout

--- MyNewApp/app/src/main/java/com/example/paramas_wae/mynewapp/MainActivity.java
+++ MyNewApp/app/src/main/java/com/example/paramas_wae/mynewapp/MainActivity.java
@@ -15,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttononClick(View v){
-        TextView textView = (TextView) findViewById(R.id.message);
-        EditText editText = (EditText) findViewById(R.id.usermessage);
-        String message =  editText.getText().toString();
-        textView.setText(message);
+        EditText editTextHeight = (EditText) findViewById(R.id.userHeight);
+        EditText editTextWeight = (EditText) findViewById(R.id.userWeight);
+        TextView textViewResult = (TextView) findViewById(R.id.userBMI);
 
+        double height = Double.parseDouble(editTextHeight.getText().toString());
+        double weight = Double.parseDouble(editTextWeight.getText().toString());
+
+        double BMI = weight / (height * height);
+
+        textViewResult.setText(Double.toString(BMI));
     }
 }
