60f3a8eb597fc762498dd2475a5773d74e3794b6
paramest.wsw <paramest.wsw@gmail.com>
2017-06-03 16:22:54 +0700

    update

--- MyDemoFragment/app/src/main/java/com/example/paramas_wae/mydemofragment/MainActivity.java
+++ MyDemoFragment/app/src/main/java/com/example/paramas_wae/mydemofragment/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.paramas_wae.mydemofragment;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FoodListFragment.FoodListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MenuDetailFragment frag = (MenuDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setMenuID(1);
+//        MenuDetailFragment frag = (MenuDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+//        frag.setMenuID(1);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        MenuDetailFragment detail = new MenuDetailFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        detail.setMenuID(id);
+        ft.replace(R.id.fragment_container,detail);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 }
