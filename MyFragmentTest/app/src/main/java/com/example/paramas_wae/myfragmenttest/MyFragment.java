622c662d23e47f852b66f72b40bdf40e1f0f6fd8
Paramest <paramest.wsw@gmail.com>
2017-06-05 14:33:58 +0700

    test fragment

---
+++ MyFragmentTest/app/src/main/java/com/example/paramas_wae/myfragmenttest/MyFragment.java
@@ -0,0 +1,37 @@
+package com.example.paramas_wae.myfragmenttest;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyFragment extends Fragment {
+
+
+    public static MyFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        MyFragment fragment = new MyFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public MyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+
+}
