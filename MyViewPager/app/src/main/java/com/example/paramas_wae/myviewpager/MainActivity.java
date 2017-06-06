99b3e5d3e2bb77b8a998704fb38001d75ff238a6
Paramest <paramest.wsw@gmail.com>
2017-06-06 12:43:02 +0700

    view pager

--- MyViewPager/app/src/main/java/com/example/paramas_wae/myviewpager/MainActivity.java
+++ MyViewPager/app/src/main/java/com/example/paramas_wae/myviewpager/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
     ViewPager pager;
     @Override
