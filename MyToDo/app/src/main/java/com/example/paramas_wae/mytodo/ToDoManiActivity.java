3cff43fe22570f69777b125a0fee0befa3e14783
Paramest <paramest.wsw@gmail.com>
2017-06-07 16:51:47 +0700

    update =w=

--- MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/ToDoManiActivity.java
+++ MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/ToDoManiActivity.java
@@ -1,11 +1,13 @@
 package com.example.paramas_wae.mytodo;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ToDoManiActivity extends AppCompatActivity {
 
@@ -24,5 +26,20 @@ public class ToDoManiActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        if(navigationView != null){
+//            setupDrawerContent(navigationView);
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
