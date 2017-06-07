b41a582f7fdf5ddde125709960491466d614e0db
Paramest <paramest.wsw@gmail.com>
2017-06-07 15:26:05 +0700

    update

--- MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/ToDoManiActivity.java
+++ MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/ToDoManiActivity.java
@@ -1,5 +1,7 @@
 package com.example.paramas_wae.mytodo;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 
 public class ToDoManiActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,9 @@ public class ToDoManiActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
         ab.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 }
