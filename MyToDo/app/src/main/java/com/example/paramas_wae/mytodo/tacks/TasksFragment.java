df82fcacb442c96056c6fcf4fdada2384803745f
Paramest <paramest.wsw@gmail.com>
2017-06-13 10:53:15 +0700

    update

--- MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/tacks/TasksFragment.java
+++ MyToDo/app/src/main/java/com/example/paramas_wae/mytodo/tacks/TasksFragment.java
@@ -1,11 +1,15 @@
 package com.example.paramas_wae.mytodo.tacks;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.paramas_wae.mytodo.R;
 
@@ -14,7 +18,15 @@ import com.example.paramas_wae.mytodo.R;
  */
 public class TasksFragment extends Fragment {
 
-    public static TasksFragment newInstance() { return new TasksFragment(); }
+    private TextView noTasksMainView;
+    private TextView noTasksAddView;
+    private TextView filteringLabelView;
+    private ImageView noTasksIcon;
+
+    public static TasksFragment newInstance() {
+        return new TasksFragment();
+    }
+
     public TasksFragment() {
         // Required empty public constructor
     }
@@ -27,4 +39,28 @@ public class TasksFragment extends Fragment {
         return inflater.inflate(R.layout.tasks_fragment, container, false);
     }
 
+    private static class TasksAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 }
+
+
