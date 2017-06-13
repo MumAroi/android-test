a9dc8ab8b0e27b542559329af614a34f48b0c898
Paramest <paramest.wsw@gmail.com>
2017-06-13 15:04:08 +0700

    database

---
+++ MyDatabaseApp/app/src/main/java/com/example/paramas_wae/mydatabaseapp/MyDatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.paramas_wae.mydatabaseapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by paramas_wae on 6/13/2017.
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "tasks.db";
+    public static final String TABLE_TASKS = "tesks";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TASKNAME = "taskname";
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_TASKS + " ( " + COLUMN_ID + " INTEGER PRIMARY AUTOINCREMENT, " + COLUMN_TASKNAME + " TEXT );";
+        db.execSQL(query);
+    }
+
+    public void addTasks(Tasks tasks) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TASKNAME, tasks.get_taskname());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_TASKS, null, values);
+        db.close();
+    }
+
+    public void removeTasks(int _id) {
+        String query = "DELETE FROM " + TABLE_TASKS + " WHERE " + COLUMN_ID + " = " + _id + ";";
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
