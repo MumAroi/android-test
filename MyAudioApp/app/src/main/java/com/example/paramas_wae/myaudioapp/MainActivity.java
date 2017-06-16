5b28373e9b29dd19260efa47c031f6ba11b33931
Paramest <paramest.wsw@gmail.com>
2017-06-16 13:11:56 +0700

    my app play audio

--- MyAudioApp/app/src/main/java/com/example/paramas_wae/myaudioapp/MainActivity.java
+++ MyAudioApp/app/src/main/java/com/example/paramas_wae/myaudioapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.paramas_wae.myaudioapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View view){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.turkish_dance);
+        mp.start(); // paly media
     }
 }
