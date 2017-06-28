bf6a428eb2327c09e25fe14b0e076887bbb95cd4
Paramest <paramest.wsw@gmail.com>
2017-06-28 13:31:59 +0700

    camera test

--- MyNewCameraApp/app/src/main/java/com/example/paramas_wae/mynewcameraapp/MainActivity.java
+++ MyNewCameraApp/app/src/main/java/com/example/paramas_wae/mynewcameraapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.paramas_wae.mynewcameraapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private File output = null;
+    private static final int CONTENT_REQEST = 1745;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        output = new File(dir,"CameraContentDemo.jpeg");
+        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
+        startActivityForResult(i,CONTENT_REQEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CONTENT_REQEST){
+            if (requestCode == RESULT_OK){
+                Intent i = new Intent(Intent.ACTION_VIEW);
+
+                i.setDataAndType(Uri.fromFile(output),"imge/jpeg");
+                startActivity(i);
+                finish();
+            }
+        }
+//        super.onActivityResult(requestCode, resultCode, data);
     }
 }
